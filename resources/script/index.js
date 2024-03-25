62681d8c2586ac08a486f316a7ffb94acbc2e1e2
anantiikenna <ananti.ikenna@gmail.com>
2024-03-25 08:53:41 +0100

    Added new project box for Nike Shoes site; updated HTML, CSS, and JS to include new modal and image; updated arrays to include new elements.
    This commit adds a new project box for the Nike Shoes site, which includes an image and a link to the site. The HTML, CSS, and JS files have been updated to include the new modal and image, as well as updated arrays to include the new elements. The new project box is hidden by default and can be displayed when the corresponding link is clicked. The close button on the new modal also works as expected.

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,27 +1,32 @@
 let view1 = document.getElementById('view1');
 let view2 = document.getElementById('view2');
 let view3 = document.getElementById('view3');
+let view4 = document.getElementById('view4');
 let vImage1 = document.getElementById('vimage1');
 let vImage2 = document.getElementById('vimage2');
 let vImage3 = document.getElementById('vimage3');
+let vImage4 = document.getElementById('vimage4');
 let img1 = document.getElementById("img01");
 let img2 = document.getElementById("img02");
 let img3 = document.getElementById("img03");
+let img4 = document.getElementById("img04");
 let captionText1 = document.getElementById("caption1");
 let captionText2 = document.getElementById("caption2");
 let captionText3 = document.getElementById("caption3");
+let captionText4 = document.getElementById("caption4");
 
 // Get the <closeX> element that closes the modal
 let closeX1 = document.getElementsByClassName("close")[0];
 let closeX2 = document.getElementsByClassName("close")[1];
 let closeX3 = document.getElementsByClassName("close")[2];
+let closeX4 = document.getElementsByClassName("close")[3];
 
 // Setting up the Array of each needed element
-let viewArray = [view1, view2, view3];
-let vImgArray = [vImage1, vImage2, vImage3];
-let imgArray = [img1, img2, img3];
-let captionTextArray = [captionText1, captionText2, captionText3];
-let closeXArray = [closeX1, closeX2, closeX3];
+let viewArray = [view1, view2, view3, view4];
+let vImgArray = [vImage1, vImage2, vImage3, vImage4];
+let imgArray = [img1, img2, img3, img4];
+let captionTextArray = [captionText1, captionText2, captionText3, captionText4];
+let closeXArray = [closeX1, closeX2, closeX3, closeX4];
 
 // Creating a function that executes each Click element.
 let call = () => {
